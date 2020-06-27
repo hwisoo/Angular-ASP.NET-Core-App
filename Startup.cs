11b363b30751be648dd14a399b2adbd448369c54
James Cho <hwisoocho@gmail.com>
2020-06-27 18:16:15 -0500

    add service and configure in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Summaries.Data;
 
 namespace Summaries
 {
@@ -27,6 +28,8 @@ namespace Summaries
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddTransient<IBookService, BookService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
