96c03deb4b3dda7ea4974658000ca9ba3c91841b
James Cho <hwisoocho@gmail.com>
2020-06-29 01:33:06 -0500

    adding new data from angular

--- ClientApp/src/app/components/new-book/new-book.component.ts
+++ ClientApp/src/app/components/new-book/new-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { FormsModule, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-book',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-book.component.css']
 })
 export class NewBookComponent implements OnInit {
+  
+  addBookForm: FormGroup;
 
-  constructor() { }
+  constructor(private service: BookService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.addBookForm = this.fb.group({
+      id:[Math.floor(Math.random() * 1000)],
+      title: [null, Validators.required],
+      author: [null, Validators.required],
+      description: [null, Validators.compose([Validators.required, Validators.minLength(30)])],
+      rating: [null],
+      dateStart: [null],
+      dateRead:[null]
+    })
+  }
+
+  onSubmit() {
+    this.service.addBook(this.addBookForm.value).subscribe(data => {
+      console.log("Submitted...");
+      this.router.navigate(["/books"]);
+    })
   }
 
 }
